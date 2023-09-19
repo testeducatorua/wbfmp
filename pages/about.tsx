65d8f5d8db426319f31b44a049e61ca38a67ea0c
testeducatorua <testeducatorua@gmail.com>
2023-09-19 21:59:59 +0300

    create about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,8 @@
+import Image from 'next/image'
+
 export default function About() {
-    return (<div>About</div>);
-  }
+    return (
+    // <div>About</div>
+    <Image src="/evildoc.jpg" width={550} height={400} className='rounded-full'/>
+    );
+}
